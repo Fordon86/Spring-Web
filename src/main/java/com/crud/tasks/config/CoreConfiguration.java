49c2af0c4a704a87793aa25d311be79c1ff71d0b
Fordon <fordon-86@o2.pl>
2020-06-17 22:17:33 +0200

    Task 23.2

--- src/main/java/com/crud/tasks/config/CoreConfiguration.java
+++ src/main/java/com/crud/tasks/config/CoreConfiguration.java
@@ -39,4 +39,5 @@ public class CoreConfiguration implements WebMvcConfigurer {
         registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 
+
 }
